870c4542959b646b5487a015947d76922998f160
lolligoo <lolligoo@outlook.com>
2024-08-07 14:15:49 +0800

    add custom bg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,19 +9,29 @@ export default {
         "132": "48rem",
       },
       height: {
-        "17": "68px",
+        "17": "70px",
         "65": "260px",
       },
       weight: {
-        "17": "68px",
+        "17": "70px",
+        "65": "260px",
       },
       marginRight: {
         "45": "18px",
       },
       backgroundImage: {
-        'slot': "url('/images/inv_slot.png')",
-        'l': "url('/images/bg.jpg')",
-      }
+        slot: "url('/images/inv_slot.png')",
+        l: "url('/images/bg.jpg')",
+        health: "url('/images/health.png')",
+        hunger: "url('/images/hunger.png')",
+        sanity: "url('/images/sanity.png')",
+        perishtime: "url('/images/perishtime.png')",
+        cooktime: "url('/images/cooktime.png')",
+        foodtype: "url('/images/foodtype.png')",
+      },
+      fontWeight: {
+        m: "520",
+      },
     },
   },
   plugins: [],
